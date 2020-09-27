ffef1082bb3bb49a7c377c5dd96c80722996e70d
George Carder <georgercarder@gmail.com>
2020-09-26 22:26:05 -0700

    start session

--- node/handler.go
+++ node/handler.go
@@ -2,11 +2,12 @@ package node
 
 import (
 	"fmt"
+	"io"
 
 	"github.com/libp2p/go-libp2p-core/network"
 )
 
-// TODO
+// TODO in progress
 
 // will handle streams
 
@@ -21,7 +22,6 @@ func StreamHandler(s network.Stream) {
 }
 
 func handleStream(s network.Stream) (err error) {
-	// TODO
 	authed := G_SSHMgr.IsAuthorized(s.Conn().RemotePeer())
 	if !authed {
 		err = fmt.Errorf("handleStream: "+
@@ -29,6 +29,40 @@ func handleStream(s network.Stream) (err error) {
 			s.Conn().RemotePeer().Pretty())
 		return
 	}
+	return startSession(s)
+}
 
+func startSession(s network.Stream) (err error) {
+	sh := initShell()
+	go func() {
+		// send stream back stdout, stderr
+		_, er := io.Copy(s, sh)
+		if er != nil {
+			if er != io.EOF {
+				// TODO LOG
+				return
+			}
+		}
+	}()
+	_, err = io.Copy(sh, s)
+	return
+}
+
+func initShell() (s *shell) {
+	// TODO
+	return
+}
+
+type shell struct {
+	// TODO
+}
+
+func (s *shell) Read(b []byte) (n int, err error) {
+	// TODO
+	return
+}
+
+func (s *shell) Write(b []byte) (n int, err error) {
+	// TODO
 	return
 }
