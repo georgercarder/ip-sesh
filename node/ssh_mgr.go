d6409258feaba9292b3b284745db39a52d18680d
George Carder <georgercarder@gmail.com>
2020-10-04 10:41:14 -0700

    mod init for SSHMgr

--- node/ssh_mgr.go
+++ node/ssh_mgr.go
@@ -2,19 +2,34 @@ package node
 
 import (
 	"crypto/ed25519"
+	"fmt"
 	"sync"
 
+	mi "github.com/georgercarder/mod_init"
 	"github.com/georgercarder/same"
 )
+
+func G_SSHMgr() (n *SSHMgr) {
+	nn, err := modInitializerSSHMgr.Get()
+	if err != nil {
+		//LogError.Println("G_Node:", err)
+		//reason := err
+		//SafelyShutdown(reason)
+		return
+	}
+	return nn.(*SSHMgr)
+}
+
+var modInitializerSSHMgr = mi.NewModInit(newSSHMgr,
+	ModInitTimeout, fmt.Errorf("*SSHMgr init error."))
 
 // read in ~/.ipssh/authorized_keys
 
 // TODO cache authorized keys
 
-var G_SSHMgr = newSSHMgr()
-
-func newSSHMgr() (s *SSHMgr) {
-	s = new(SSHMgr)
+func newSSHMgr() (s interface{}) { //*SSHMgr
+	ss := new(SSHMgr)
+	s = ss
 	return
 } // TODO PROPERLY INIT
 
@@ -45,7 +60,7 @@ func getPubKey(domainName string) (pk *ed25519.PublicKey) {
 
 func checkPubKeys(
 	hash []byte, nonce []byte) (pubKey *ed25519.PublicKey, ok bool) {
-	pks := G_SSHMgr.DumpPubKeys()
+	pks := G_SSHMgr().DumpPubKeys()
 	// TODO PUT IN THREADS
 	for _, pk := range pks {
 		if same.Same(hash, Hash(PubKey2Slice(pk), nonce)) {
