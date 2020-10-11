70d6954a7ee56629509f39d8c22c8e4a1f69d47e
George Carder <georgercarder@gmail.com>
2020-10-10 21:00:30 -0700

    readability

--- node/handshake.go
+++ node/handshake.go
@@ -5,74 +5,12 @@ import (
 	"crypto/ed25519"
 	"encoding/json"
 	"fmt"
-	"time"
 
-	. "github.com/georgercarder/lockless-map"
 	"github.com/georgercarder/same"
 
 	"github.com/libp2p/go-libp2p-core/peer"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 )
-
-var G_HandshakeMgr = newHandshakeMgr() // TODO USE MODINIT
-
-func newHandshakeMgr() (h *HandshakeMgr) {
-	h = new(HandshakeMgr)
-	h.DomainName2Handshake = NewLocklessMap()
-	h.Nonce2Handshake = NewLocklessMap()
-	return
-}
-
-type HandshakeMgr struct {
-	DomainName2Handshake LocklessMap // map[string]*Handshake
-	Nonce2Handshake      LocklessMap // map[string]*Handshake
-}
-
-func (m *HandshakeMgr) newHandshake(
-	hp *HandshakePacket, pubKey *ed25519.PublicKey) {
-	hs := &Handshake{
-		DomainName: hp.DomainName,
-		Nonce:      hp.Nonce,
-		Challenge:  hp.Challenge,
-		PubKey:     pubKey,
-		LastTouch:  time.Now()}
-	hs.StopChnl = make(chan bool)
-	m.DomainName2Handshake.Put(hp.DomainName, hs)
-	m.Nonce2Handshake.Put(string(hp.Nonce), hs)
-}
-
-func (m *HandshakeMgr) SendStop(domainName string) {
-	fmt.Println("debug SendStop start", domainName)
-	hs := m.DomainName2Handshake.Take(domainName)
-	if hs != nil {
-		fmt.Println("debug SendStop")
-		stop := true
-		hhs := hs.(*Handshake)
-		hhs.StopChnl <- stop
-	}
-}
-
-func (m *HandshakeMgr) Stop(domainName string) <-chan bool {
-	hs := m.DomainName2Handshake.Take(domainName)
-	if hs == nil {
-		// TODO LOG
-		// this should not!! happen
-		return make(chan bool) // empty chnl
-	}
-	hhs := hs.(*Handshake)
-	return hhs.StopChnl
-}
-
-type Handshake struct {
-	DomainName string
-	Nonce      []byte
-	Challenge  []byte
-	PubKey     *ed25519.PublicKey
-	StopChnl   (chan bool)
-	LastTouch  time.Time
-}
-
-// TODO
 
 // client
 
@@ -91,54 +29,7 @@ func StartHandshake(domainName string) {
 	go publishUntilChallenge(hp)
 }
 
-const publishInterval = 1 * time.Second // TODO put elsewhere and tune
-
-type HandshakePacket struct {
-	DomainName string
-	Nonce      []byte
-	Hash       []byte
-	Challenge  []byte
-	Signature  []byte
-	PubKey     *ed25519.PublicKey
-}
-
-func (hp *HandshakePacket) Bytes() (b []byte) {
-	b, err := json.Marshal(hp)
-	if err != nil {
-		// TODO LOG ERR
-	}
-	return
-}
-
-func Slice2HandshakePacket(b []byte) (hp *HandshakePacket, err error) {
-	hp = new(HandshakePacket)
-	fmt.Println("debug Slice2HandshakePacket", b)
-	err = json.Unmarshal(b, &hp)
-	return
-}
-
-func publishUntilChallenge(hp *HandshakePacket) {
-	pubCH := make(chan bool)
-	go func() {
-		for {
-			pubCH <- true
-			time.Sleep(publishInterval)
-		}
-	}()
-	stopper := G_HandshakeMgr.Stop(hp.DomainName)
-	fmt.Println("debug stopper", stopper)
-	for {
-		select {
-		case <-pubCH:
-			Publish(hp.DomainName, hp.Bytes())
-		case <-stopper:
-			return
-		}
-	}
-}
-
 // 3. respond to challenge
-// TODO
 func prepareChallengeResponse(
 	hp *HandshakePacket) (res *HandshakePacket, err error) {
 	// hp does not have domain name (it could but server can cheat),
@@ -208,4 +99,5 @@ func checkAndRespondToAlert(domainName string, a []byte) {
 }
 
 // 4. validate challenge response, put pid in active screens
-// TODO
+// see // 3 of stream_handler.go
+// func checkHandshakeResponse(s network.Stream) (err error)
---
+++ node/handshake_utils.go
@@ -0,0 +1,114 @@
+package node
+
+import (
+	"crypto/ed25519"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	. "github.com/georgercarder/lockless-map"
+)
+
+var G_HandshakeMgr = newHandshakeMgr() // TODO USE MODINIT
+
+func newHandshakeMgr() (h *HandshakeMgr) {
+	h = new(HandshakeMgr)
+	h.DomainName2Handshake = NewLocklessMap()
+	h.Nonce2Handshake = NewLocklessMap()
+	return
+}
+
+type HandshakeMgr struct {
+	DomainName2Handshake LocklessMap // map[string]*Handshake
+	Nonce2Handshake      LocklessMap // map[string]*Handshake
+}
+
+func (m *HandshakeMgr) newHandshake(
+	hp *HandshakePacket, pubKey *ed25519.PublicKey) {
+	hs := &Handshake{
+		DomainName: hp.DomainName,
+		Nonce:      hp.Nonce,
+		Challenge:  hp.Challenge,
+		PubKey:     pubKey,
+		LastTouch:  time.Now()}
+	hs.StopChnl = make(chan bool)
+	m.DomainName2Handshake.Put(hp.DomainName, hs)
+	m.Nonce2Handshake.Put(string(hp.Nonce), hs)
+}
+
+func (m *HandshakeMgr) SendStop(domainName string) {
+	fmt.Println("debug SendStop start", domainName)
+	hs := m.DomainName2Handshake.Take(domainName)
+	if hs != nil {
+		fmt.Println("debug SendStop")
+		stop := true
+		hhs := hs.(*Handshake)
+		hhs.StopChnl <- stop
+	}
+}
+
+func (m *HandshakeMgr) Stop(domainName string) <-chan bool {
+	hs := m.DomainName2Handshake.Take(domainName)
+	if hs == nil {
+		// TODO LOG
+		// this should not!! happen
+		return make(chan bool) // empty chnl
+	}
+	hhs := hs.(*Handshake)
+	return hhs.StopChnl
+}
+
+type Handshake struct {
+	DomainName string
+	Nonce      []byte
+	Challenge  []byte
+	PubKey     *ed25519.PublicKey
+	StopChnl   (chan bool)
+	LastTouch  time.Time
+}
+
+const publishInterval = 1 * time.Second // TODO put elsewhere and tune
+
+type HandshakePacket struct {
+	DomainName string
+	Nonce      []byte
+	Hash       []byte
+	Challenge  []byte
+	Signature  []byte
+	PubKey     *ed25519.PublicKey
+}
+
+func (hp *HandshakePacket) Bytes() (b []byte) {
+	b, err := json.Marshal(hp)
+	if err != nil {
+		// TODO LOG ERR
+	}
+	return
+}
+
+func Slice2HandshakePacket(b []byte) (hp *HandshakePacket, err error) {
+	hp = new(HandshakePacket)
+	fmt.Println("debug Slice2HandshakePacket", b)
+	err = json.Unmarshal(b, &hp)
+	return
+}
+
+func publishUntilChallenge(hp *HandshakePacket) {
+	pubCH := make(chan bool)
+	go func() {
+		for {
+			pubCH <- true
+			time.Sleep(publishInterval)
+		}
+	}()
+	stopper := G_HandshakeMgr.Stop(hp.DomainName)
+	fmt.Println("debug stopper", stopper)
+	for {
+		select {
+		case <-pubCH:
+			Publish(hp.DomainName, hp.Bytes())
+		case <-stopper:
+			return
+		}
+	}
+}
